ad109fb82e9032b9b1013394f8b358c7c6abdf19
Letinemeso <sh0n556@bk.ru>
2024-02-16 05:24:55 +0300

    fixed bunch of memory leaks

--- source/Physical_Models/Physical_Model_2D.cpp
+++ source/Physical_Models/Physical_Model_2D.cpp
@@ -27,18 +27,18 @@ void Physical_Model_2D::M_update_rectangular_border()
         m_current_border.bottom = polygon[0].y;
     }
 
-	for(unsigned int i=0; i<m_polygons_count; ++i)
+    for(unsigned int i=0; i<m_polygons_count; ++i)
     {
         const Polygon& polygon = *m_polygons_holder->get_polygon(i);
 
-		for(unsigned int p=0; p<3; ++p)
+        for(unsigned int p=0; p<3; ++p)
         {
             if(m_current_border.left > polygon[p].x) m_current_border.left = polygon[p].x;
             if(m_current_border.right < polygon[p].x) m_current_border.right = polygon[p].x;
             if(m_current_border.top < polygon[p].y) m_current_border.top = polygon[p].y;
             if(m_current_border.bottom > polygon[p].y) m_current_border.bottom = polygon[p].y;
-		}
-	}
+        }
+    }
 }
 
 glm::vec3 Physical_Model_2D::M_calculate_center_of_mass() const
@@ -56,7 +56,7 @@ glm::vec3 Physical_Model_2D::M_calculate_center_of_mass() const
 
 const LEti::Geometry_2D::Rectangular_Border& Physical_Model_2D::curr_rect_border() const
 {
-	return m_current_border;
+    return m_current_border;
 }
 
 
@@ -68,50 +68,51 @@ Physical_Model_2D::Physical_Model_2D()
 
 Physical_Model_2D::Physical_Model_2D(const Physical_Model_2D& _other)
 {
-	setup(_other.m_raw_coords, _other.m_raw_coords_count, _other.m_collision_permissions);
-	copy_real_coordinates(_other);
+    setup(_other.m_raw_coords, _other.m_raw_coords_count, _other.m_collision_permissions);
+    copy_real_coordinates(_other);
 }
 
 void Physical_Model_2D::setup(const float* _raw_coords, unsigned int _raw_coords_count, const bool* _collision_permissions)
 {
-	delete[] m_raw_coords;
+    delete[] m_raw_coords;
 
-	m_raw_coords_count = _raw_coords_count;
-	m_raw_coords = new float[m_raw_coords_count];
-	for (unsigned int i = 0; i < m_raw_coords_count; ++i)
-		m_raw_coords[i] = _raw_coords[i];
+    m_raw_coords_count = _raw_coords_count;
+    m_raw_coords = new float[m_raw_coords_count];
+    for (unsigned int i = 0; i < m_raw_coords_count; ++i)
+        m_raw_coords[i] = _raw_coords[i];
 
-	delete[] m_collision_permissions;
+    delete[] m_collision_permissions;
 
-	m_collision_permissions = new bool[m_raw_coords_count / 3];
-	for(unsigned int i=0; i<m_raw_coords_count / 3; ++i)
-		m_collision_permissions[i] = _collision_permissions[i];
+    m_collision_permissions = new bool[m_raw_coords_count / 3];
+    for(unsigned int i=0; i<m_raw_coords_count / 3; ++i)
+        m_collision_permissions[i] = _collision_permissions[i];
 
     delete[] m_polygons_holder;
     m_polygons_holder = M_create_polygons_holder();
 
     m_polygons_count = m_raw_coords_count / 9;
     m_polygons_holder->allocate(m_polygons_count);
-	for (unsigned int i = 0; i < m_polygons_count; ++i)
+    for (unsigned int i = 0; i < m_polygons_count; ++i)
         m_polygons_holder->get_polygon(i)->setup(&m_raw_coords[i * 9], &m_collision_permissions[i * 3]);
 }
 
 void Physical_Model_2D::move_raw(const glm::vec3 &_stride)
 {
-	for(unsigned int i=0; i<m_raw_coords_count; i += 3)
-	{
-		m_raw_coords[i] += _stride.x;
-		m_raw_coords[i + 1] += _stride.y;
-		m_raw_coords[i + 2] += _stride.z;
-	}
+    for(unsigned int i=0; i<m_raw_coords_count; i += 3)
+    {
+        m_raw_coords[i] += _stride.x;
+        m_raw_coords[i + 1] += _stride.y;
+        m_raw_coords[i + 2] += _stride.z;
+    }
 
-	for(unsigned int i=0; i < m_polygons_count; ++i)
+    for(unsigned int i=0; i < m_polygons_count; ++i)
         m_polygons_holder->get_polygon(i)->calculate_center();
 }
 
 Physical_Model_2D::~Physical_Model_2D()
 {
-	delete[] m_raw_coords;
+    delete[] m_raw_coords;
+    delete[] m_collision_permissions;
     delete m_polygons_holder;
 }
 
@@ -120,21 +121,21 @@ void Physical_Model_2D::update(const glm::mat4x4& _matrix)
 {
     L_ASSERT(m_polygons_holder);
 
-	for (unsigned int i = 0; i < m_polygons_count; ++i)
+    for (unsigned int i = 0; i < m_polygons_count; ++i)
         m_polygons_holder->get_polygon(i)->update_points_with_single_matrix(_matrix);
 
-	M_update_rectangular_border();
+    M_update_rectangular_border();
     m_center_of_mass = M_calculate_center_of_mass();
 }
 
 void Physical_Model_2D::copy_real_coordinates(const Physical_Model_2D &_other)
 {
-	for (unsigned int i = 0; i < m_polygons_count; ++i)
-	{
-		for(unsigned int points_i = 0; points_i < 3; ++points_i)
+    for (unsigned int i = 0; i < m_polygons_count; ++i)
+    {
+        for(unsigned int points_i = 0; points_i < 3; ++points_i)
             m_polygons_holder->get_polygon(i)[points_i] = _other.m_polygons_holder->get_polygon(i)[points_i];
-	}
-	m_current_border = _other.m_current_border;
+    }
+    m_current_border = _other.m_current_border;
 }
 
 
@@ -159,7 +160,7 @@ const Polygon_Holder_Base* Physical_Model_2D::get_polygons() const
 
 unsigned int Physical_Model_2D::get_polygons_count() const
 {
-	return m_polygons_count;
+    return m_polygons_count;
 }
 
 
