3f9dab7cd128850dd179fe783d160697e495845d
Артём <sh0n556@bk.ru>
2025-04-24 00:24:38 +0300

    updated polygon vs ray intersection check func

--- source/Collision_Detection/Primitives/Polygon_VS_Ray_Intersection.cpp
+++ source/Collision_Detection/Primitives/Polygon_VS_Ray_Intersection.cpp
@@ -5,48 +5,37 @@
 using namespace LPhys;
 
 
-Polygon_VS_Ray_Intersection_Data LPhys::ray_intersects_polygon(const glm::vec3& _start, const glm::vec3& _direction, const Polygon& _polygon, float _tolerance)       //  note to self: this code was mostly written by chat-gpt, don't try to remember what it does
+Polygon_VS_Ray_Intersection_Data LPhys::ray_intersects_polygon(const glm::vec3& _start, const glm::vec3& _direction, const Polygon& _polygon, float _tolerance)       //  note to self: this code was mostly written by deepseek, don't try to remember what it does
 {
     Polygon_VS_Ray_Intersection_Data result;
 
-    glm::vec3 edgeAB = _polygon[1] - _polygon[0];
-    glm::vec3 edgeAC = _polygon[2] - _polygon[0];
-    glm::vec3 directionCrossEdgeAC =  LEti::Math::cross_product(_direction, edgeAC);
-    float determinant = LEti::Math::dot_product(edgeAB, directionCrossEdgeAC);
+    glm::vec3 edge1 = _polygon[1] - _polygon[0];
+    glm::vec3 edge2 = _polygon[2] - _polygon[0];
 
-    if (LEti::Math::floats_are_equal(determinant, 0.0f))
+    glm::vec3 pvec = LEti::Math::cross_product(_direction, edge2);
+    float det = LEti::Math::dot_product(edge1, pvec);
+
+    if (det < _tolerance && det > -_tolerance)
         return result;
 
-    float inverseDeterminant = 1.0f / determinant;
-    glm::vec3 distanceFromAtoLineStart = _start - _polygon[0];
-    float u = inverseDeterminant * LEti::Math::dot_product(distanceFromAtoLineStart, directionCrossEdgeAC);
+    float inv_det = 1.0f / det;
 
-    if (u < 0.0 || u > 1.0)
+    glm::vec3 tvec = _start - _polygon[0];
+    float u = LEti::Math::dot_product(tvec, pvec) * inv_det;
+    if (u < 0.0f || u > 1.0f)
         return result;
 
-    glm::vec3 distanceCrossEdgeAB = LEti::Math::cross_product(distanceFromAtoLineStart, edgeAB);
-    float v = inverseDeterminant * LEti::Math::dot_product(_direction, distanceCrossEdgeAB);
-
-    if (v < 0.0 || u + v > 1.0)
+    glm::vec3 qvec = LEti::Math::cross_product(tvec, edge1);
+    float v = LEti::Math::dot_product(_direction, qvec) * inv_det;
+    if (v < 0.0f || u + v > 1.0f)
         return result;
 
-    float t = inverseDeterminant * LEti::Math::dot_product(edgeAC, distanceCrossEdgeAB);
+    float t = dot(edge2, qvec) * inv_det;
 
-    if (t <= 1e-6)
+    if(t < 0.0f)
         return result;
 
-    glm::vec3 intersectionPoint = _start + _direction * t;
-    result.intersection = point_is_inside_polygon(intersectionPoint, _polygon, _tolerance);
-    if(!result)
-        return result;
-
-    glm::vec3 direction_to_intersection_point = intersectionPoint - _start;
-    for(unsigned int i=0; i<3; ++i)
-    {
-        if(_direction[i] < 0.0f && direction_to_intersection_point[i] >= 0.0f)
-            return result;
-    }
-
-    result.point = intersectionPoint;
+    result.intersection = true;
+    result.point = _start + _direction * t;
     return result;
 }
